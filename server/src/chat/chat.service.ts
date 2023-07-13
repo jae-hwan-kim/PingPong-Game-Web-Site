b32cbf12375ff95627f947bbfc889b16779ca234
김재환 <woghks7859@gmail.com>
2023-07-14 01:12:22 +0900

    TEST: Try change Raw Query to Query Builder #10

--- server/src/chat/chat.service.ts
+++ server/src/chat/chat.service.ts
@@ -1,39 +1,49 @@
 import { Inject, Injectable, NotFoundException } from '@nestjs/common';
-import { Repository, getConnection } from 'typeorm';
+import { Repository } from 'typeorm';
 import { Channel, ChannelMember, Message } from './chat.entity';
-import { CreateChatDMDto, CreateChatDto, FindDMChannelDto } from './dto/chats.dto';
+import {
+  CreateChatDMDto,
+  CreateChatDto,
+  FindDMChannelDto,
+} from './dto/chats.dto';
 
 @Injectable()
 export class ChatService {
   constructor(
+    @Inject('DATA_SOURCE') private dataSource,
     @Inject('MESSAGES_REPOSITORY')
     private messageRepository: Repository<Message>,
     @Inject('CHANNELS_REPOSITORY')
     private channelRepository: Repository<Channel>,
     @Inject('CHANNELMEMBERS_REPOSITORY')
     private channelMemberRepository: Repository<ChannelMember>,
-  ) { }
+  ) {}
 
   // async createChannel(createChannelDto: CreateChatDto){}
 
   // situation 1. DM 채팅방이 없을 때 행위자 user1 channelType 0 is DM
-  async createDMChannel(createChatDMDto: CreateChatDMDto, target_nickname: number) {
+  async createDMChannel(
+    createChatDMDto: CreateChatDMDto,
+    target_nickname: number,
+  ) {
     const { userIdx, channelType, message } = createChatDMDto;
     const socketClinetUserId = 0; // 당소. 나중에 client로부터 받아올 예정
-    let targetUser: number; // 귀소, 일단은 지금 유저 정보가 없어서 식별자 number 값으로 대체
-    
-    targetUser = 1;
-    const channelMember = await this.channelMemberRepository.findOne({ 
-      where: { userIdx: userIdx, channelType: channelType }
+    const targetUser = 1; // 귀소, 일단은 지금 유저 정보가 없어서 식별자 number 값으로 대체
+
+    const channelMember = await this.channelMemberRepository.findOne({
+      where: { userIdx: userIdx, channelType: channelType },
     });
-    
-    if (channelMember) { // 존재하면 안 돼서 에러를 반환.
-      throw new NotFoundException(`ChannelMember with userIdx ${userIdx} already exists`);
+
+    if (channelMember) {
+      // 존재하면 안 돼서 에러를 반환.
+      throw new NotFoundException(
+        `ChannelMember with userIdx ${userIdx} already exists`,
+      );
     }
     // 채널 생성 먼저
     // 이거 용도, 한 채널을 생성한 뒤에 그 채널에 대한 두 가지의 채널멤버 튜플을 넣어야해서.
     const channelMaxId = await this.channelRepository
-      .createQueryBuilder("channel")
+      .createQueryBuilder('channel')
       .select('MAX(channel.id)', 'id')
       .getRawOne();
     let idx = 1;
@@ -41,7 +51,7 @@ export class ChatService {
     // 이렇게 넣으면 nullalbe 해지나?
     const channel = await this.channelRepository.save({
       channelIdx: idx,
-      channelName : "DM",
+      channelName: 'DM',
       channelType: 0,
     });
 
@@ -67,31 +77,39 @@ export class ChatService {
     });
 
     return { channel, channelMember1, channelMember2, DM };
+  }
 
-  };
-
-  async findDMChannel(my_user : number, target_user : number) {
+  async findDMChannel(my_user: number, target_user: number) {
     // my_user 와 target_user 의 idx 가 존재하는
     // 채널 참여자 테이블을 찾는다.(idx는 채널 참여자 테이블의 userIdx)
-    
-    const query = `
-      SELECT *
-      FROM channel_member
-      WHERE ("userIdx" = $1 AND "channelId" IN (
-          SELECT "channelId"
-          FROM "channel_member"
-          WHERE "userIdx" = $2 AND "channelType" = 0
-      ))
-      OR ("userIdx" = $2 AND "channelId" IN (
-          SELECT "channelId"
-          FROM "channel_member"
-          WHERE "userIdx" = $1 AND "channelType" = 0
-      ))
-    `;
-    const parameters = [my_user, target_user];
-    
-    return this.channelMemberRepository.query(query, parameters);
-  };
+
+    const query = await this.dataSource
+      .getRepository(ChannelMember)
+      .createQueryBuilder('cm')
+      .where('cm.userIdx = :my_user', { my_user: my_user })
+      .select('cm.channelId', 'channelId')
+      .getRawMany();
+    // 추가 구문이 필요하다. '채널 타입이 0인 것만 찾아야 한다.' 등
+    return query;
+
+    // const query = `
+    //   SELECT *
+    //   FROM channel_member
+    //   WHERE ("userIdx" = $1 AND "channelId" IN (
+    //       SELECT "channelId"
+    //       FROM "channel_member"
+    //       WHERE "userIdx" = $2 AND "channelType" = 0
+    //   ))
+    //   OR ("userIdx" = $2 AND "channelId" IN (
+    //       SELECT "channelId"
+    //       FROM "channel_member"
+    //       WHERE "userIdx" = $1 AND "channelType" = 0
+    //   ))
+    // `;
+    // const parameters = [my_user, target_user];
+
+    // return this.channelMemberRepository.query(query, parameters);
+  }
 
   /*
   async findDMChannel(my_user : number, target_user : number) {
@@ -119,47 +137,46 @@ export class ChatService {
             WHERE userId = 'User1' AND channelType = 0
         ));
 */
+}
 
-};
+// 예시
+// createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
+//   return this.boardRepository.createBoard(createBoardDto, user);
+// }
 
-  // 예시
-  // createBoard(createBoardDto: CreateBoardDto, user: User): Promise<Board> {
-  //   return this.boardRepository.createBoard(createBoardDto, user);
-  // }
+// createChannelandInitDM(createChatDMDto:CreateChatDMDto, target_nickname: string, content: string) {
+//   // 임의 ID 정수 값 넣기
+//   // channelRepository.find({ where: { userNick = target_nickname } })
+//   const { channelIdx, userIdx, channelType } = createChatDMDto;
+//   // const { channelName, owner, password } = anythingDto(target_nickname, owner, password);
+//   this.channelRepository.createChannel({ // 주입의 문제 있음.
+//       channelName: target_nickname,
+//       owner: userIdx,
+//       password: null,
+//   });
+//   this.channelMemberRepository.createChannelMember(
+//    );
+//   this.messageRepository.createMessage();
+//   // return
+// }
 
-  // createChannelandInitDM(createChatDMDto:CreateChatDMDto, target_nickname: string, content: string) {
-  //   // 임의 ID 정수 값 넣기
-  //   // channelRepository.find({ where: { userNick = target_nickname } })
-  //   const { channelIdx, userIdx, channelType } = createChatDMDto;
-  //   // const { channelName, owner, password } = anythingDto(target_nickname, owner, password);
-  //   this.channelRepository.createChannel({ // 주입의 문제 있음.
-  //       channelName: target_nickname,
-  //       owner: userIdx,
-  //       password: null,
-  //   });
-  //   this.channelMemberRepository.createChannelMember(
-  //    );
-  //   this.messageRepository.createMessage();
-  //   // return 
-  // }
+// ------------------------------------------------------
+// create(createChatDto: CreateChatDto) {
+//   return 'This action adds a new chat';
+// }
 
-  // ------------------------------------------------------
-  // create(createChatDto: CreateChatDto) {
-  //   return 'This action adds a new chat';
-  // }
+// findAll() {
+//   return `This action returns all chat`;
+// }
 
-  // findAll() {
-  //   return `This action returns all chat`;
-  // }
+// findOne(id: number) {
+//   return `This action returns a #${id} chat`;
+// }
 
-  // findOne(id: number) {
-  //   return `This action returns a #${id} chat`;
-  // }
+// update(id: number, updateChatDto: UpdateChatDto) {
+//   return `This action updates a #${id} chat`;
+// }
 
-  // update(id: number, updateChatDto: UpdateChatDto) {
-  //   return `This action updates a #${id} chat`;
-  // }
-
-  // remove(id: number) {
-  //   return `This action removes a #${id} chat`;
-  // }
+// remove(id: number) {
+//   return `This action removes a #${id} chat`;
+// }
