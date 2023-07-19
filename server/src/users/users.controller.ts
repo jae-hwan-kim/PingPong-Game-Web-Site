4ced5e3db7ea75180163e0ec2675dcfd17919d57
김재환 <woghks7859@gmail.com>
2023-07-19 13:59:50 +0900

    FEAT: Add API generating user for testing, cuz we do not implement Oauth yet

--- server/src/users/users.controller.ts
+++ server/src/users/users.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-};
+  @Post('signup')
+  signUp(@Body() userCreadentailsDto: any): Promise<void> {
+    console.log('userCreadentailsDto: ', userCreadentailsDto);
+    return this.usersService.signUp(userCreadentailsDto);
+  }
+}
--- server/src/users/users.service.ts
+++ server/src/users/users.service.ts
@@ -1,7 +1,13 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Users } from './users.entity';
 import { UserDto } from './dto/users.dto';
+import { CreateUserDto } from './dto/create-user.dto';
 
 @Injectable()
 export class UsersService {
@@ -15,5 +21,23 @@ export class UsersService {
       where: [{ nickname: nickname }],
     });
     return user.idx;
+  }
+
+  // async 가 필요한가?
+  async signUp(userCreadentailsDto: CreateUserDto): Promise<void> {
+    // return await this.createUser(userCreadentailsDto);
+    return this.createUser(userCreadentailsDto);
+  }
+
+  async createUser(userCreadentailsDto: CreateUserDto): Promise<void> {
+    const { intra, nickname } = userCreadentailsDto;
+
+    const user = this.userRepository.create({ intra, nickname });
+    try {
+      await this.userRepository.save(user);
+    } catch (error) {
+      throw new InternalServerErrorException();
+      // console.log('error', error);
+    }
   }
 }
